be7ab174e31242643d868ae71bec0b3dcc23c733
Rocio Jacob <rochijacob@gmail.com>
2022-04-19 19:29:30 -0300

    [Fix]NoHayDatos

--- src/components/CardsContainer/Cardscontainer.js
+++ src/components/CardsContainer/Cardscontainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 //Typography
-import { Spin, Button } from 'antd'
+import { Spin, Button, Alert } from 'antd'
 import SingleCard from './Card/SingleCard'
 
 
@@ -23,11 +23,14 @@ export class Cardscontainer extends Component {
           <Button type='primary' onClick={() => this.props.bringMore()} style={{margin: 10}}>
             Traer mas peliculas
           </Button>
-          <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', flexDirection: this.props.card ? 'column' : 'row'}}>
-            {results.map((result, id) => (
-              <SingleCard key={id} result={result} delete={(id, title) => this.props.delete(id, title)} card={this.props.cards}/>
-          ))}
-          </div>
+          {results.length > 0 ? 
+                    <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', flexDirection: this.props.card ? 'column' : 'row'}}>
+                    {results.map((result, id) => (
+                      <SingleCard key={id} result={result} delete={(id, title) => this.props.delete(id, title)} card={this.props.cards}/>
+                  ))}
+                  </div> :
+                  <Alert message='No hemos encontrado ese titulo' type='error' />
+           }
 
       </div>
     )
